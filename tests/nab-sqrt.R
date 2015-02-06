d9b685a63616cd52491ff63df1af2f3f495ddf47
hb <hb@aroma-project.org>
2015-02-06 11:44:47 -0800

    Tweaks

--- tests/nab-sqrt.R
+++ tests/nab-sqrt.R
@@ -4,22 +4,27 @@ opts <- options(warn=1L)
 
 n <- 1e6
 
-message("TEST: sqrt()")
+message("TEST #1: sqrt()")
 foo <- function() {
   x <- runif(n)
+  message("named(x): ", named(x))
   message("address(x): ", ax <- address(x))
   y <- sqrt(x)
+  message("named(x): ", named(x))
+  message("named(y): ", named(y))
   message("address(y): ", ay <- address(y), " != address(x)")
   stopifnot(ay != ax)
 }
 foo()
 print(gc())
 
-message("TEST: sqrt() with nab internally")
+message("TEST #1: sqrt() with nab internally")
 foo <- function() {
   x <- runif(n)
+  message("named(x): ", named(x))
   message("address(x): ", ax <- address(x))
   y <- sqrt(nab(x))
+  message("named(y): ", named(y))
   message("address(y): ", ay <- address(y), " == address(x)")
   stopifnot(ay == ax)
 }
@@ -28,32 +33,40 @@ print(gc())
 
 
 
-message("TEST: sqrt()")
+message("TEST #2: sqrt()")
 local({
   x <- runif(n)
+  message("named(x): ", named(x))
   message("address(x): ", ax <- address(x))
   y <- sqrt(x)
+  message("named(x): ", named(x))
+  message("named(y): ", named(x))
   message("address(y): ", ay <- address(y), " != address(x)")
   stopifnot(ay != ax)
 })
 print(gc())
 
-message("TEST: sqrt() with nab locally")
+message("TEST #2: sqrt() with nab locally")
 local({
   x <- runif(n)
+  message("named(x): ", named(x))
   message("address(x): ", ax <- address(x))
   y <- sqrt(nab(x))
+  message("named(y): ", named(x))
   message("address(y): ", ay <- address(y), " == address(x)")
   stopifnot(ay == ax)
 })
 print(gc())
 
 
-message("TEST: sqrt()")
+message("TEST #3: sqrt()")
 foo <- function(x) {
+  message("named(x): ", named(x))
   message("address(x): ", ax <- address(x))
   str(x)
   y <- sqrt(x)
+  message("named(x): ", named(x))
+  message("named(y): ", named(y))
   message("address(y): ", ay <- address(y), " != address(x)")
   stopifnot(ay != ax)
 }
@@ -61,10 +74,12 @@ x <- runif(n)
 foo(x)
 print(gc())
 
-message("TEST: sqrt() with nab in call")
+message("TEST #3: sqrt() with nab in call")
 foo <- function(x) {
+  message("named(x): ", named(x))
   message("address(x): ", ax <- address(x))
   y <- sqrt(x)
+  message("named(y): ", named(y))
   message("address(y): ", ay <- address(y), " == address(x)")
   ## FIXME:
   ## stopifnot(ay == ax)
